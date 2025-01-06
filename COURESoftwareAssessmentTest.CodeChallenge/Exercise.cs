4ac0eb17ea193cbdffeba38ba4660ffe97b4a13e
Abdulhamiid Oyebo <oyeboharemu@yahoo.com>
2025-01-06 13:39:11 +0100

    Completed the code challenge

---
+++ COURESoftwareAssessmentTest.CodeChallenge/Exercise.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COURESoftwareAssessmentTest.CodeChallenge
+{
+    public class Exercise
+    {
+
+        public int Question1(int[] arr)
+        {
+            int point = 0;
+            foreach (int i in arr) 
+            { 
+                if(i == 8)
+                {
+                    point += 5;
+                }
+
+                if(i %  2 == 0)
+                {
+                    point += 1;
+                }
+                else
+                {
+                    point += 3;
+                }
+            }
+            return point;
+        }
+    }
+}
--- COURESoftwareAssessmentTest.CodeChallenge/Program.cs
+++ COURESoftwareAssessmentTest.CodeChallenge/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using COURESoftwareAssessmentTest.CodeChallenge;
+
+
 
 //
+var exercise = new Exercise();
+
+Console.WriteLine("Examples:");
+
+Console.WriteLine($"Input: [1,2,3,4,5] \n Output: {exercise.Question1(new int[] { 1, 2, 3, 4, 5 })} ");
+Console.WriteLine($"Input: [15,25,35] \n Output: {exercise.Question1(new int[] { 15, 25, 35 })} ");
+Console.WriteLine($"Input: [15,25,35] \n Output: {exercise.Question1(new int[] { 8, 8 })} ");
+
