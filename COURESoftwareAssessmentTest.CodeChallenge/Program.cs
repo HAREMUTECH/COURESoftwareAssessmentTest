3667fabc809854b0242461bf7988c96d3668bc4b
Abdulhamiid Oyebo <oyeboharemu@yahoo.com>
2025-01-07 09:56:37 +0100

    Update

--- COURESoftwareAssessmentTest.CodeChallenge/Program.cs
+++ COURESoftwareAssessmentTest.CodeChallenge/Program.cs
@@ -8,7 +8,7 @@ var exercise = new Exercise();
 
 Console.WriteLine("Examples:");
 
-Console.WriteLine($"Input: [1,2,3,4,5]\n Output: {exercise.Question1(new int[] { 1, 2, 3, 4, 5 })} ");
-Console.WriteLine($"Input: [15,25,35]\n Output: {exercise.Question1(new int[] { 15, 25, 35 })} ");
-Console.WriteLine($"Input: [8,8]\n Output: {exercise.Question1(new int[] { 8, 8 })} ");
+Console.WriteLine($"Input: [1,2,3,4,5]\nOutput: {exercise.Question1(new int[] { 1, 2, 3, 4, 5 })} ");
+Console.WriteLine($"Input: [15,25,35]\nOutput: {exercise.Question1(new int[] { 15, 25, 35 })} ");
+Console.WriteLine($"Input: [8,8]\nOutput: {exercise.Question1(new int[] { 8, 8 })} ");
 
